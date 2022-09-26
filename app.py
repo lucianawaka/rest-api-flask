a1420ce9c792dec0b796376e456952939eea72de
Luciana Bahia Wakabayashi Fabri <lucianawakabayashi@gmail.com>
2022-09-26 15:17:04 -0300

    POST Request

--- app.py
+++ app.py
@@ -11,9 +11,22 @@ stores = [
                 "price": 15.99
             }
         ]
+    },
+    {
+        "name": "PMGV Store",
+        "items": [
+            {
+                "name": "PC",
+                "price": 2515.99
+            }
+        ]
     }
 ]
 
 @app.route("/store") # http://127.0.0.1:5000/store
 def get_stores():
+    return {"stores": stores}
+
+@app.route("/store", methods=['POST'])
+def create_store():
     return {"stores": stores}
